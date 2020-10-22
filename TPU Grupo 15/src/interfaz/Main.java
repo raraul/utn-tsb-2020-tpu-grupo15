0421bd3b61ed72b498d03dbd463d79cd31287636
raraul <raulranalletti+raraul@live.com>
2020-10-22 01:31:23 -0300

    Agregado DirectoryChooser para origen de datos

--- TPU Grupo 15/src/interfaz/Main.java
+++ TPU Grupo 15/src/interfaz/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("principal.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Elecciones PASO 2019");
+        primaryStage.setScene(new Scene(root, 600, 300));
         primaryStage.show();
     }
 
--- TPU Grupo 15/src/interfaz/PrincipalController.java
+++ TPU Grupo 15/src/interfaz/PrincipalController.java
@@ -1,4 +1,24 @@
 package interfaz;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
+
 public class PrincipalController {
+    public Label lblOrigenDatosRuta;
+
+    public void cambiarUbicacion(ActionEvent actionEvent) {
+        DirectoryChooser dc = new DirectoryChooser();
+        dc.setTitle("Seleccionar ubicación de los datos");
+        if (!lblOrigenDatosRuta.getText().equals("Seleccionar ubicación de los datos")) {
+            dc.setInitialDirectory(new File(lblOrigenDatosRuta.getText()));
+        }
+
+        File carpetaOrigenDatos = dc.showDialog(null);
+        if (carpetaOrigenDatos != null) {
+            lblOrigenDatosRuta.setText(carpetaOrigenDatos.getPath());
+        }
+    }
 }
